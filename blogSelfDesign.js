1aa30a43d9b1e6ad2ec1bb50c2bbf3346a8af878
Slacr <11812766+Fslacr@user.noreply.gitee.com>
2023-04-06 02:23:56 +0800

    mydesign

--- blogSelfDesign.js
+++ blogSelfDesign.js
@@ -1,3 +1,13 @@
+/*
+ * @Author: Slacr 11812766+Fslacr@user.noreply.gitee.com
+ * @Date: 2023-04-05 22:41:28
+ * @LastEditors: Slacr 11812766+Fslacr@user.noreply.gitee.com
+ * @LastEditTime: 2023-04-06 00:17:00
+ * @FilePath: \undefinedc:\Users\28453\Desktop\gitHubRepo\MyBlog - 副本\themes\inside\source\blogSelfDesign.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 $(function () {
-    $(".vmeta>span.vat").html("reply")
+    window.onload = function () {
+        $(".vmeta>span.vat").html("reply")
+    }
 })
